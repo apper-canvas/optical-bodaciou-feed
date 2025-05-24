95da1fe391151cdfedb85174ee509e4952133710
Vivek-Gotraj <123731082+Vivek-Gotraj@users.noreply.github.com>
2025-05-24 16:00:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+     allowedHosts: true,
+     host: true,
+     strictPort: true,
+     port: 5173
+ }
 })
